ed68f7a9f5d4413067ed1da8ea54dbe4b4078c7e
tac33 <collins.tyrese99@gmail.com>
2024-08-13 19:28:49 -0400

    Added item class, game state methods, updated player class.

--- src/com/tac33/player/Player.java
+++ src/com/tac33/player/Player.java
@@ -1,5 +1,52 @@
 package com.tac33.player;
 
-public class Player {
+import com.tac33.item.Item;
+import com.tac33.pokemon.species.generic.Pokemon;
 
+public class Player {
+	private Pokemon[] pokebox = new Pokemon[30];
+	private int money;
+	private Item[] inventory = new Item[50];
+	
+	/*
+	 * Add new pokemon to pokebox
+	 */
+	public void addToPokebox(Pokemon p) {
+		
+		for(int i=0; i < pokebox.length; i++) {
+			
+			if (pokebox[i] == null) {
+				pokebox[i] = p;
+				System.out.println("Added to box");
+			} else {
+				System.out.println("pokebox is full");
+			}
+		}
+	}
+	
+	/*
+	 * Set pokemon in specific pokebox index
+	 */
+	public void setInPokebox(int i, Pokemon p) {
+		pokebox[i] = p;
+	}
+	
+	
+	/*
+	 * Swap pokemon positions in pokebox
+	 */
+	public void replacePokemonInPokebox(int i, int x) {
+		Pokemon temp;
+		
+		temp = pokebox[i];
+		pokebox[i] = pokebox[x];
+		pokebox[x] = temp;
+		
+	}
+	
+	public Pokemon[] getPokebox() {
+		
+		return pokebox;
+	}
+	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 import java.util.Scanner;
 
+import com.tac33.player.Player;
 import com.tac33.pokemon.breeding.Breed;
 import com.tac33.pokemon.species.generic.*;
 import com.tac33.pokemon.species.*;
@@ -31,19 +32,25 @@ public class Main {
 		 */
 	}
 	
+	/*
+	 * Main menu GameState
+	 *  Start - Start a new game
+	 *  Load - load a previously saved game
+	 *  Quit - Quit this menu
+	 */
 	public static void mainMenu() {
-		System.out.println("Welcome to Pokemon Breeding Mimic!");
-		System.out.println("1. Start Game \n2. Load \n3. Quit");
 		boolean menuCheck = false;
 		
 		while(!menuCheck) {
+			System.out.println("Welcome to Pokemon Breeding Mimic!");
+			System.out.println("1. Start Game \n2. Load \n3. Quit");
 			try {
 				Scanner input = new Scanner(System.in);
 				int choice = input.nextInt();
 				if (choice == 1) {
 					System.out.println("Game started");
-					menuCheck = true;
-					StartMenu();
+					Player user = chooseStarter();
+					gameMenu(user);
 				} else if (choice == 2) {
 					System.out.println("Loading game...");
 					menuCheck = true;
@@ -52,7 +59,7 @@ public class Main {
 					menuCheck = true;
 				} 
 				else {
-					System.out.println("Invalid input, please try again.");
+					System.out.println("Invalid input. Use integers 1-3.");
 				}
 				
 				if (menuCheck == true) {
@@ -61,41 +68,141 @@ public class Main {
 				}
 				
 			} catch(Exception e) {
-				System.out.println("Invalid Input. Must be an integer");
+				System.out.println("Invalid input, please use an integer "  +  e);
 			}
 		}
 	}
 	
-	public static void StartMenu() {
+	/*
+	 * Main play GameState
+	 * - your pokemon - get all pokemon currrently in your box
+	 * - Iventory - get all items in your inventory
+	 * - Item Shop - buy items
+	 * - Pokemon Shop - buy pokemon
+	 * - Quit - quit to main menu
+	 */
+	public static void gameMenu(Player user) {
 		boolean menuCheck = false;
 		
-		System.out.println("1. Pokemon \n2. ItemShop \n3. Iventory \n4. Quit");
 		while(!menuCheck) {
+			System.out.println("1. Your Pokemon \n2. Iventory \n3. Item Shop \n4. Pokemon Shop \n5. Quit");
 			try {
 				Scanner input = new Scanner(System.in);
 				int choice = input.nextInt();
 				
 				if (choice == 1) {
-					System.out.println("Get Pokemon Menu");
-					menuCheck = true;
+					pokemonState(user);
 				} else if (choice == 2) {
-					System.out.println("Get Itemshop");
-					menuCheck = true;
+					inventoryState(user);
 				} else if (choice == 3) {
-					System.out.println("Get Inventory");
-					menuCheck = true;
+					itemShopState(user);
 				} else if (choice == 4) {
+					pokemonShopState(user);
+				} else if (choice == 5) {
 					System.out.println("Quitting");
+					menuCheck = true;
 				}
 				else {
-					System.out.println("Invalid input, please try again.");
+					System.out.println("Invalid input. Use integers 1-5.");
 				}
 				
 				
 			} catch(Exception e) {
-				System.out.println("Invalid input, please try again.");
+				System.out.println("Invalid input, please use an integer");
 			}
 		}
+	}
+	
+	/*
+	 * Choose starter pokemon for the beginning of the game.
+	 */
+	public static Player chooseStarter() {
+		
+		boolean isValid = false;
+		Player user = new Player();
+		
+		System.out.println("Choose your starting pokemon!");
+		System.out.println("1. Bulbasaur \n2. Charmander \n3. Squirtle");
+		Scanner input = new Scanner(System.in);
+		
+		while(!isValid) {
+			
+			try {
+				int choice = input.nextInt();
+				if (choice == 1) {
+					Pokemon poke1 = new Bulbasaur();
+					Pokemon poke2 = new Bulbasaur();
+					poke1.setGender("male");
+					poke2.setGender("female");
+					System.out.println("You chose Bulbasaur!");
+					
+					user.setInPokebox(0, poke1);
+					user.setInPokebox(1, poke2);
+					isValid = true;
+					
+				} else if (choice == 2) {
+					Pokemon poke1 = new Charmander();
+					Pokemon poke2 = new Charmander();
+					poke1.setGender("male");
+					poke2.setGender("female");
+					System.out.println("You chose Charmander!");
+					
+					user.setInPokebox(0, poke1);
+					user.setInPokebox(1, poke2);
+					isValid = true;
+					
+				} else if (choice == 3) {
+					Pokemon poke1 = new Squirtle();
+					Pokemon poke2 = new Squirtle();
+					poke1.setGender("male");
+					poke2.setGender("female");
+					System.out.println("You chose Squirtle!");
+					
+					user.setInPokebox(0, poke1);
+					user.setInPokebox(1, poke2);
+					isValid = true;
+				} else {
+					System.out.println("Invalid input. Use integers 1-3.");
+				}
+				
+			} catch (Exception e) {
+				System.out.println("Invalid input, please use an integer");
+			}
+			
+		}
+		
+		return user;
+		
+	}
+	
+	public static void pokemonState(Player user) {
+		boolean isValid = false;
+		
+		System.out.println("get Pokemon Menu");
+		Pokemon[] pokebox = user.getPokebox();
+		
+		for(int i=0; i < pokebox.length; i++) {
+			System.out.println(i + " " + pokebox[i].getSpecies());
+		}
+		
+		
+		
+		while(!isValid) {
+			
+			
+		}
+	}
+	
+	public static void inventoryState(Player user) {
+		
+	}
+	
+	public static void itemShopState(Player user) {
+		
+	}
+	
+	public static void pokemonShopState(Player user) {
+		
 	}
 
 }
